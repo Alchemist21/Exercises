d24e1ce8842efa1abacc909624bbe8f770ef236b
Louell Sala <35514769+Alchemist21@users.noreply.github.com>
2018-07-23 13:47:07 -0400

    Add files via upload

---
+++ apartments.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 21 11:45:53 2018
+
+@author: Me
+"""
+portfolio = {'IBM':'100', 
+             'MSFT':'200', 
+             'INTC':'20', 
+             'SNAP':'30', 
+             'GOOG': '500'}
+
+for i in portfolio.items():
+    print("key:  " + k)
+    print("value: " + str(v))
