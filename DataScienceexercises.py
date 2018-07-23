07a2004677b964366b17027d848fde49dad5df34
Achemist21 <35514769+Alchemist21@users.noreply.github.com>
2018-07-23 12:59:14 -0400

    Add files via upload

---
+++ DataScienceexercises.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 21 10:55:15 2018
+
+@author: Me
+"""
+
+logins = {'john':'john123','sue':'sure123'}
+employees = {'1':'john', '2':'sue'}
+print(logins[employees[1]])
